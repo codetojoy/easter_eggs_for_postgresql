c1b3117da5d587f4c0e4df6d87b1fadf9d5ffb5e
Michael Easter <codetojoy@gmail.com>
2025-05-02 04:57:53 -0300

    incremental update Fri  2 May 2025 04:57:53 ADT

--- egg_merge_books_MAY_2025/merge.sql
+++ egg_merge_books_MAY_2025/merge.sql
@@ -5,8 +5,8 @@ ON loc.address = li.address AND loc.city = li.city AND loc.province = li.provinc
 WHEN MATCHED THEN
   UPDATE set version = loc.version + 1, updated_on = default
 WHEN NOT MATCHED THEN
-  INSERT (address, city, province, version)
-  VALUES (li.address, li.city, li.province, 1);
+  INSERT (address, city, province)
+  VALUES (li.address, li.city, li.province);
 
 MERGE INTO book b
 USING book_incoming bi
@@ -14,7 +14,7 @@ ON b.author = bi.author AND b.title = bi.title
 WHEN MATCHED THEN
   UPDATE set version = b.version + 1, updated_on = default
 WHEN NOT MATCHED THEN
-  INSERT (author, title, version)
-  VALUES (bi.author, bi.title, 1);
+  INSERT (author, title)
+  VALUES (bi.author, bi.title);
 
 
--- egg_merge_books_MAY_2025/setup.sql
+++ egg_merge_books_MAY_2025/setup.sql
@@ -1,9 +1,10 @@
 
-DROP TABLE IF EXISTS location_incoming;
+DROP TABLE IF EXISTS book_location;
+DROP TABLE IF EXISTS book;
 DROP TABLE IF EXISTS location;
 
+DROP TABLE IF EXISTS location_incoming;
 DROP TABLE IF EXISTS book_incoming;
-DROP TABLE IF EXISTS book;
 
 CREATE TABLE location_incoming (
     address VARCHAR(255),
@@ -16,7 +17,7 @@ CREATE TABLE location (
     address VARCHAR(255),
     city VARCHAR(255),
     province VARCHAR(255),
-    version INTEGER,
+    version INTEGER DEFAULT 1,
     created_on TIMESTAMP DEFAULT current_timestamp,
     updated_on TIMESTAMP DEFAULT current_timestamp
 );
@@ -30,7 +31,20 @@ CREATE TABLE book (
     book_id  SERIAL PRIMARY KEY,
     title VARCHAR(255),
     author VARCHAR(255),
-    version INTEGER,
+    version INTEGER DEFAULT 1,
     created_on TIMESTAMP DEFAULT current_timestamp,
     updated_on TIMESTAMP DEFAULT current_timestamp
 );
+
+CREATE TABLE book_location (
+    book_id  INTEGER,
+    location_id INTEGER,
+    active BOOLEAN DEFAULT true,
+    version INTEGER DEFAULT 1,
+    created_on TIMESTAMP DEFAULT current_timestamp,
+    updated_on TIMESTAMP DEFAULT current_timestamp
+);
+
+ALTER TABLE book_location ADD FOREIGN KEY(book_id) REFERENCES book(book_id);
+ALTER TABLE book_location ADD FOREIGN KEY(location_id) REFERENCES location(location_id);
+
