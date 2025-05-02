99d000cff99ad62881dcb72d66875ba766c15d8d
Michael Easter <codetojoy@gmail.com>
2025-05-02 20:06:26 -0300

    rename files

--- egg_merge_books_MAY_2025/go.sh
+++ egg_merge_books_MAY_2025/go.sh
@@ -4,15 +4,15 @@ set -e
 
 ./run-file.sh setup.sql $MY_DB
 
-./run-file.sh incoming-v1.sql $MY_DB
+./run-file.sh incoming-iter1.sql $MY_DB
 ./run-file.sh merge.sql $MY_DB
 
 sleep 1
-./run-file.sh incoming-v2.sql $MY_DB
+./run-file.sh incoming-iter2.sql $MY_DB
 ./run-file.sh merge.sql $MY_DB
 
 sleep 1
-./run-file.sh incoming-v3.sql $MY_DB
+./run-file.sh incoming-iter3.sql $MY_DB
 ./run-file.sh merge.sql $MY_DB
 
 ./run-file.sh list-table.sql $MY_DB
