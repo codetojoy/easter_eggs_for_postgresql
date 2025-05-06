9a108d06647388ffb52ee95e978d31a431f30ca6
Michael Easter <codetojoy@gmail.com>
2025-05-05 21:57:11 -0300

    incremental update Mon  5 May 2025 21:57:11 ADT

--- egg_vacuum_MAY_2025/go-step2.sh
+++ egg_vacuum_MAY_2025/go-step2.sh
@@ -5,7 +5,7 @@ set -e
 echo "deleting ..."
 ./run-file.sh delete-rows.sql $MY_DB
 
-# echo "vacuuming ..."
-# ./run-file.sh vacuum-table.sql $MY_DB
+echo "vacuuming ..."
+./run-file.sh vacuum-table.sql $MY_DB
 
 ./run-file.sh table-size.sql $MY_DB
