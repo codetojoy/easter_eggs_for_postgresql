2e7424dd763df9a6a9749ac8b947552ba52b651b
Michael Easter <codetojoy@gmail.com>
2025-05-23 11:14:51 -0300

    incremental update Fri 23 May 2025 11:14:51 ADT

--- egg_udemy_MAY_2025/json/sandbox.sql
+++ egg_udemy_MAY_2025/json/sandbox.sql
@@ -16,3 +16,26 @@ first_name,
 )
 from directors
 order by director_id desc;
+
+-- e.g. vid 247
+
+select count(*) from contacts_docs;
+select * from contacts_docs limit 5;
+
+-- query
+select count(*) from contacts_docs
+where body @> '{"first_name":"John"}'
+
+-- explain query
+explain analyze select count(*) from contacts_docs
+where body @> '{"first_name":"John"}'
+
+-- create index
+CREATE INDEX IF NOT EXISTS idx_contacts_docs_body ON contacts_docs USING GIN(body);
+
+-- create index v2
+CREATE INDEX IF NOT EXISTS idx_contacts_docs_body_v2 ON contacts_docs USING GIN(body jsonb_path_ops);
+
+-- size
+SELECT pg_size_pretty(pg_relation_size('idx_contacts_docs_body'::regclass)) as index_size
+SELECT pg_size_pretty(pg_relation_size('idx_contacts_docs_body_v2'::regclass)) as index_size
